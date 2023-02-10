2c285d7a35c368ec5c567dcc12f8107f359f0545
Hauke Bartsch <HaukeBartsch@gmail.com>
2023-02-10 10:09:23 +0100

    check two types of triggers, for study and for series

--- trigger/trigger.py
+++ trigger/trigger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from datetime import datetime
+import re, sys, os
 
 # continuously check folder specified in config.json
 config = {
@@ -24,13 +25,28 @@ while True:
     obj = os.scandir(config["arrived"])
     for file in obj:
         if file.is_file():
+            # lets parse the filename and see if we get all structured fields
+            re_series = "([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+)"
+            re_study = "([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+)"
+            x = re.match(re_series, file.name)
+            type = "series"
+            if x == None:
+                x = re.match(re_study, file.name)
+                type = "study"
+            aet = x[1]
+            aec = x[2]
+            dummy = x[3]
+            StudyInstanceUID = x[4]
+            if type == "series":
+                SeriesInstanceUID = x[5]
+ 
             mtime = os.path.getmtime(file)
             t = datetime.fromtimestamp(t)
             now = datetime.now()
             delta = now - t
-            if (delta.total_seconds() > config["timeout"]:
+            if delta.total_seconds() > config["timeout"]:
                 # now trigger the action
-                print("Trigger the action now")
+                print("Trigger the action now for type: %s" % (type))
 
                 # remove the .arrived file again
                 pass

f22355bfeedf0b581a868b583f9d3dfcf1644361
Hauke Bartsch <HaukeBartsch@gmail.com>
2024-08-13 16:10:23 +0200

    placeholder log viewer

---
+++ log_viewer/js/dashboard.js
@@ -0,0 +1,51 @@
+/* globals Chart:false */
+
+(() => {
+  'use strict'
+
+  // Graphs
+  const ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'rgba(255, 159, 64, 0.9)',
+        borderColor: '#ffffff',
+        borderRadius: 25,
+        borderWidth: 1,
+        barThickness: 50,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false
+        },
+        tooltip: {
+          boxPadding: 3
+        }
+      }
+    }
+  })
+})()

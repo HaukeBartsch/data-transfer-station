553d93e86f9db0c36da69c37260a7840613dfdc6
Hauke Bartsch <HaukeBartsch@gmail.com>
2025-06-25 15:02:45 +0200

    create if not there yet

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@
 # should be run by processing user
 
 # create the directories required to run
-test -d /data/site/archive && sudo mkdir -p /data/site/archive
-test -d /data/site/raw && sudo mkdir -p /data/site/raw
-test -d /data/site/.arrived && sudo mkdir -p /data/site/.arrived
-test -d /data/code && sudo mkdir -p /data/code
-test -d /data/logs && sudo mkdir -p /data/logs
-test -d /data/proc && sudo mkdir -p /data/proc
+test -d /data/site/archive || sudo mkdir -p /data/site/archive
+test -d /data/site/raw || sudo mkdir -p /data/site/raw
+test -d /data/site/.arrived || sudo mkdir -p /data/site/.arrived
+test -d /data/code || sudo mkdir -p /data/code
+test -d /data/logs || sudo mkdir -p /data/logs
+test -d /data/proc || sudo mkdir -p /data/proc
 sudo chmod -R 755 /data
 
 # We should not be root here?

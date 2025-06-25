65a948b60830ba3097a713dac84139da63659d9d
Hauke Bartsch <HaukeBartsch@gmail.com>
2025-06-25 17:05:42 +0200

    make log directory writable by all

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ test -d /data/site/archive || sudo mkdir -m 755 -p /data/site/archive
 test -d /data/site/raw || sudo mkdir -m 755 -p /data/site/raw
 test -d /data/site/.arrived || sudo mkdir -m 755 -p /data/site/.arrived
 test -d /data/code || sudo mkdir -m 755 -p /data/code
-test -d /data/logs || sudo mkdir -m 755 -p /data/logs
+test -d /data/logs || sudo mkdir -m 777 -p /data/logs
 test -d /data/proc || sudo mkdir -m 755 -p /data/proc
 
 # We should not be root here?

eb5e49f031d304a1a553ee30d70f3a857c3fae48
Hauke Bartsch <HaukeBartsch@gmail.com>
2025-06-25 15:00:46 +0200

    set permissions

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-# create the directories required to run
-test -d /data/site/archive && mkdir -p /data/site/archive
-test -d /data/site/raw && mkdir -p /data/site/raw
-test -d /data/site/.arrived && mkdir -p /data/site/.arrived
-test -d /data/code && mkdir -p /data/code
-test -d /data/logs && mkdir -p /data/logs
-test -d /data/proc && mkdir -p /data/proc
+# should be run by processing user
 
+# create the directories required to run
+test -d /data/site/archive && sudo mkdir -p /data/site/archive
+test -d /data/site/raw && sudo mkdir -p /data/site/raw
+test -d /data/site/.arrived && sudo mkdir -p /data/site/.arrived
+test -d /data/code && sudo mkdir -p /data/code
+test -d /data/logs && sudo mkdir -p /data/logs
+test -d /data/proc && sudo mkdir -p /data/proc
+sudo chmod -R 755 /data
+
+# We should not be root here?
 # docker compose build --no-cache
 docker compose build
 docker compose up
